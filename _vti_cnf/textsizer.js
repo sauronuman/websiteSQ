d154ca6460cf6617ae0ccf824bae2e42bcde7007
D Gartland <dgartland021@c2ken.net>
2024-02-27 08:11:38 +0000

    Initial commit of website files

---
+++ _vti_cnf/textsizer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Oct 2023 10:51:11 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|HTML/ContactUs.htm Template.dwt HTML/Courses.htm HTML/AboutUs.htm HTML/OurTeam.htm HTML/JobVacancies.htm index.html
+vti_author:SR|C2KEN\\squinn780
+vti_modifiedby:SR|C2KEN\\squinn780
+vti_timecreated:TR|19 Oct 2023 10:51:11 -0000
+vti_cacheddtm:TX|19 Oct 2023 10:51:11 -0000
+vti_filesize:IR|1270
